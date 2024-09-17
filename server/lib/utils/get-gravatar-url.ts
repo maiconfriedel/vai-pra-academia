da65ebe6aa5ca3332dd404ee81b20cf0f03a1fba
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-09-17 17:02:18 -0300

    refactor: changed utils functions file names

--- server/lib/utils/get-gravatar-url.ts
+++ server/lib/utils/get-gravatar-url.ts
@@ -4,5 +4,5 @@ export const getGravatarUrl = (email: string) => {
   const trimmedEmail = email.trim().toLowerCase()
   const hash = crypto.createHash('sha256').update(trimmedEmail).digest('hex')
 
-  return `https://www.gravatar.com/avatar/${hash}?s=${80}&d=identicon`
+  return `https://www.gravatar.com/avatar/${hash}?s=${400}&d=mp`
 }
