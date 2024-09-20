15905ac56d42b2cc0556afd28ee14fbb53fdd1d6
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-09-20 16:51:01 -0300

    feat: created create user form

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,13 +2,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useForm } from "@tanstack/react-form";
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { useState } from "react";
 import Textra from "react-textra";
 
 const Index = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  function handleStart() {
+    setShowForm(true);
+  }
+
   const form = useForm({
     defaultValues: {
       email: "",
       name: "",
+      password: "",
+      desiredWeeklyFrequency: 3,
     },
     onSubmit: async ({ value }) => {
       console.log(value);
@@ -16,26 +25,26 @@ const Index = () => {
   });
 
   return (
-    <>
-      <div className=" text-white h-full flex flex-row">
-        <div
-          className={`text-[48px] max-w-[40%] font-bold mr-4 h-screen bg-[url('src/assets/vai-pra-academia.jpg')] bg-cover flex flex-1 justify-start items-end`}
-        >
-          <Textra
-            data={[
-              "Defina sua meta 📈",
-              "Registre seus treinos 💪",
-              "Suba de nível 🚀",
-              "Compartilhe com os amigos 🔥",
-            ]}
-            effect="scale"
-            duration={500}
-            stopDuration={3000}
-            className="text-white m-24 drop-shadow-2xl"
-          />
-        </div>
-        <div className="flex flex-1 flex-col justify-start mr-10 ml-4 mt-20 w-full">
-          <h1 className="text-5xl font-bold mb-10">Vai pra Academia 💪</h1>
+    <div className=" text-white h-full flex flex-row">
+      <div
+        className={`text-[64px] font-bold mr-4 h-screen bg-[url('src/assets/academia.avif')] bg-cover flex flex-1 justify-start items-start`}
+      >
+        <Textra
+          data={[
+            "Defina sua meta 📈",
+            "Registre seus treinos 💪",
+            "Suba de nível 🚀",
+            "Compartilhe com os amigos 🔥",
+          ]}
+          effect="scale"
+          duration={500}
+          stopDuration={5000}
+          className="text-white m-12 drop-shadow-2x"
+        />
+      </div>
+      <div className="flex flex-1 flex-col mr-10 ml-4 mt-20 w-full h-full">
+        <h1 className="text-5xl font-bold mb-8">Vai pra Academia 💪</h1>
+        {showForm && (
           <form
             className="flex flex-1 flex-col gap-2"
             onSubmit={(e) => {
@@ -44,6 +53,24 @@ const Index = () => {
               form.handleSubmit();
             }}
           >
+            <label htmlFor="name" className="mr-2">
+              Nome
+            </label>
+            <form.Field
+              name="name"
+              children={(field) => (
+                <Input
+                  id="name"
+                  type="text"
+                  className="bg-black text-white w-full py-6"
+                  placeholder="Seu nome"
+                  required
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+              )}
+            />
             <label htmlFor="email" className="mr-2">
               E-mail
             </label>
@@ -62,17 +89,17 @@ const Index = () => {
                 />
               )}
             />
-            <label htmlFor="name" className="mr-2">
-              Nome:
+            <label htmlFor="email" className="mr-2">
+              Senha
             </label>
             <form.Field
-              name="name"
+              name="password"
               children={(field) => (
                 <Input
-                  id="name"
-                  type="text"
+                  id="password"
+                  type="password"
                   className="bg-black text-white w-full py-6"
-                  placeholder="Seu nome"
+                  placeholder="*******"
                   required
                   value={field.state.value}
                   onBlur={field.handleBlur}
@@ -80,8 +107,33 @@ const Index = () => {
                 />
               )}
             />
-
+            <label htmlFor="desiredWeeklyFrequency" className="mr-2">
+              Meta por Semana
+            </label>
+            <form.Field
+              name="desiredWeeklyFrequency"
+              children={(field) => (
+                <Input
+                  id="name"
+                  type="number"
+                  className="bg-black text-white w-full py-6"
+                  required
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.valueAsNumber)}
+                  min={1}
+                  max={7}
+                />
+              )}
+            />
             <Button className="w-full mt-4" type="submit">
+              Criar conta
+            </Button>
+          </form>
+        )}
+        {!showForm && (
+          <div className="h-full flex justify-end mb-4 flex-col">
+            <Button className="mt-4" type="submit" onClick={handleStart}>
               Começar agora 🔥
             </Button>
             <Button
@@ -92,10 +144,10 @@ const Index = () => {
             >
               <Link to="/login">Já tenho conta 💪</Link>
             </Button>
-          </form>
-        </div>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
